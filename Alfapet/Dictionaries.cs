e83e256758f581f8676d05e29e38b445d4109334
Your Name <you@example.com>
2021-04-11 14:26:05 +0200

    U

--- Alfapet/Dictionaries.cs
+++ Alfapet/Dictionaries.cs
@@ -1,29 +1,30 @@
 ﻿using Microsoft.Xna.Framework;
-using System.Diagnostics;
 using System.IO;
 using System.Collections.Generic;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Alfapet
 {
     class Dictionaries : Game
     {
         
-        public struct Word
-        {
-            public string Name;
-            public string Meaning;
-        }
+        public static Dictionary<string, string> Current;
 
         public static void Initialize(string dictionary)
         {
             string json = File.ReadAllText(@"dictionaries/" + dictionary + ".json"); // Få JSON från vilket språk man kallar funktionen
-            // TODO :: :: : :
-            string[][] words = JsonConvert.DeserializeObject<string[][]>(json);
 
-            Debug.WriteLine(words);
+            Current = JsonConvert.DeserializeObject<Dictionary<string, string>>(json); // Gör om JSON till ett dictionary
+        }
 
+        public static bool IsWord(string word)
+        {
+            return Current.ContainsKey(word);
+        }
+
+        public static string GetDefinition(string word)
+        {
+            return Current[word];
         }
     }
 }
